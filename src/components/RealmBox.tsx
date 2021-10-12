3b8fd29be536d9f92784466139587a7c5bc5ba5b
Neil Kistner <neil.kistner@gmail.com>
2021-10-12 15:08:43 -0500

    Fix for when names contain UTF-8 characters

--- src/components/RealmBox.tsx
+++ src/components/RealmBox.tsx
@@ -4,15 +4,16 @@ import { Box } from '@chakra-ui/react'
 
 type RealmBoxProps = {
   children: ReactNode
+  w?: string
 }
 
-export default function RealmBox({ children }: RealmBoxProps) {
+export default function RealmBox({ children, w = '55vw' }: RealmBoxProps) {
   return (
     <Box
       borderLeft="1px solid #ffb30080"
       borderTop="2px solid #ffb300"
       boxShadow="10px 9px 0 #444"
-      w="55vw"
+      w={w}
     >
       {children}
     </Box>
